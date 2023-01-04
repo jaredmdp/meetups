41104c7c1ccdd2100aba0808c7c3c894c16a9592
Jared Mandap <jaredmandap@gmail.com>
2023-01-03 19:29:34 -0600

    first commit

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,12 @@
+//domain/new-meetup
+import NewMeetupForm from '../../components/meetups/NewMeetupForm'
+
+function NewMeetupPage() {
+    function addMeetupHandler(data) {
+        console.log(data);
+    }
+
+    return <NewMeetupForm onAddMeetup={addMeetupHandler} />
+}
+
+export default NewMeetupPage;
